aae96a632b87f71da457f41b72bf411de6fc2360
徐涛 <midnite@outlook.com>
2023-03-08 09:17:53 +0800

    fix(layout):修复基本布局。

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import { NotificationsProvider } from "@mantine/notifications";
-import React, { FC, useState } from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { appRouter } from "./router";
-import { useAppTheme } from "./theme";
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { useColorScheme } from '@mantine/hooks';
+import { Notifications } from '@mantine/notifications';
+import React, { FC, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { appRouter } from './router';
+import { useAppTheme } from './theme';
 
 const AppMain: FC = () => {
   const preferredColorScheme = useColorScheme();
@@ -14,17 +16,12 @@ const AppMain: FC = () => {
     <React.StrictMode>
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={setColorScheme}>
         <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-          <NotificationsProvider position="bottom-right" limit={5} zIndex={999}>
-            <RouterProvider router={appRouter} />
-          </NotificationsProvider>
+          <Notifications position="bottom-right" limit={5} zIndex={999} />
+          <RouterProvider router={appRouter} />
         </MantineProvider>
       </ColorSchemeProvider>
     </React.StrictMode>
   );
 };
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <AppMain />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<AppMain />);
